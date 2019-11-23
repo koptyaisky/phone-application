9814e417640b1cc52b9da1bbfaf9dfae8852412a
Alina <alina@MacBook-Pro-Eugene-7.local>
2019-11-23 23:40:05 +0300

    Feature: component catalog [ref: no-ref]

---
+++ src/phones/phones-catalog/phones-catalog.component.js
@@ -0,0 +1,39 @@
+
+
+export class PhonesCatalogComponent {
+    constructor({element, phones, onPhoneSelected}) {
+        this._element = element;
+        this._phones = phones;
+        this._onPhoneSelected = onPhoneSelected;
+        this._render();
+        this._element.addEventListener("click", (event) => {
+            let el = event.target.closest('.thumb');
+            if(!el) {
+                return;
+            }
+            const {phoneId} = el.dataset;
+            this._onPhoneSelected(phoneId);
+        });
+    }
+
+    _render() {
+        this._element.innerHTML = `
+        <ul class="phones">
+          ${this._phones.map((phone) => `
+            <li class="thumbnail">
+                <a href="#!/phones/${phone.id}" class="thumb" data-phone-id="${phone.id}">
+                  <img alt="${phone.name}" src="${phone.imageUrl}">
+                </a>
+                
+                <div class="phones__btn-buy-wrapper">
+                  <a class="btn btn-success">Add</a>
+                </div>
+                
+                <a href="#!/phones/${phone.id}" class="show-details">${phone.name}</a>
+                <p>${phone.snippet}</p>
+            </li>
+          `).join('')}
+        </ul>
+        `
+    }
+}
---
+++ src/phones/phones.component.js
@@ -0,0 +1,57 @@
+import {PhonesCatalogComponent} from "./phones-catalog/phones-catalog.component.js";
+import {PhonesServise} from "./phones.service.js";
+
+
+export class PhonesComponent {
+    constructor({element}) {
+        this._element = element;
+        this._render();
+
+        new PhonesCatalogComponent({
+            element: this._element.querySelector('.phones-catalog'),
+            phones: PhonesServise.getAll(),
+            onPhoneSelected: (phoneId) => {
+                console.log(phoneId);
+            },
+        });
+    }
+
+    _render() {
+        this._element.innerHTML = `
+        <div class="row">
+        
+          <!--Sidebar-->
+          <div class="col-md-2">
+            <section>
+              <p>
+                Search:
+                <input>
+              </p>
+        
+              <p>
+                Sort by:
+                <select>
+                  <option value="name">Alphabetical</option>
+                  <option value="age">Newest</option>
+                </select>
+              </p>
+            </section>
+        
+            <section>
+              <p>Shopping Cart</p>
+              <ul>
+                <li>Phone 1</li>
+                <li>Phone 2</li>
+                <li>Phone 3</li>
+              </ul>
+            </section>
+          </div>
+        
+          <!--Main content-->
+          <div class="col-md-10">
+            <div class="phones-catalog"></div>
+          </div>
+        </div>
+        `
+    }
+}
